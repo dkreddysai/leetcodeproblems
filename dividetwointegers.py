d3cd32571b0ebf1243241d8038af3d6e0caa8235
N.DEVA KUMAR REDDY <128282107+dkreddysai@users.noreply.github.com>
2023-07-26 18:04:22 +0530

    Add files via upload

---
+++ dividetwointegers.py
@@ -0,0 +1,18 @@
+class Solution():
+    def divide(dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+        if dividend*divisor>0:
+            res=abs(dividend)//abs(divisor)
+        else:
+            res=-(abs(dividend)//abs(divisor))
+        intmin=-(2**31)
+        intmax=(2**31)-1
+        return res if intmin<=res<=intmax else intmax
+    if __name__ == '__main__':
+        d = divide(18,3)
+        print(d)
+    
