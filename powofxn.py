8ed64bdd3cddd217aa98cc617d57abe00b133abd
N.DEVA KUMAR REDDY <128282107+dkreddysai@users.noreply.github.com>
2023-07-27 22:58:08 +0530

    Add files via upload

---
+++ powofxn.py
@@ -0,0 +1,14 @@
+#https://leetcode.com/problems/powx-n/
+#Pow(x, n)
+class Solution():
+    def myPow(x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        return x**n
+    x=2.10000
+    n=3
+    print(myPow(x,n))
+        
