b83a0d2bb162635eb2a15209df81048a9b701958
N.DEVA KUMAR REDDY <128282107+dkreddysai@users.noreply.github.com>
2023-08-14 21:24:05 +0530

    Update trackingand_identification.py

--- trackingand_identification.py
+++ trackingand_identification.py
@@ -2,7 +2,7 @@ import cv2
 
 def main():
     # Initialize video capture
-    video_path = ''
+    video_path = 'highwaycars.mp4'
     cap = cv2.VideoCapture('highwaycars.mp4')
 
 
@@ -31,10 +31,10 @@ def main():
 
         if success:
             x, y, w, h = map(int, new_bbox)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 20, 0), 2)
-            cv2.putText(frame, "Tracked", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 20, 0), 2)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 200, 0), 2)
+            cv2.putText(frame, "Tracked", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 200, 0), 2)
         else:
-            cv2.putText(frame, "Lost", (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 20), 2)
+            cv2.putText(frame, "Lost", (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 200), 2)
 
         cv2.imshow("Object Tracking", frame)
 
