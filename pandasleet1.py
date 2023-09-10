76c21f25e16901ed68ec68e7d5c26f0aa6e63e69
N.DEVA KUMAR REDDY <128282107+dkreddysai@users.noreply.github.com>
2023-09-10 22:43:50 +0530

    Add files via upload

---
+++ pandasleet1.py
@@ -0,0 +1,12 @@
+import pandas as pd
+data = [['Afghanistan', 'Asia', 652230, 25500100, 20343000000], ['Albania', 'Europe', 28748, 2831741, 12960000000], ['Algeria', 'Africa', 2381741, 37100000, 188681000000], ['Andorra', 'Europe', 468, 78115, 3712000000], ['Angola', 'Africa', 1246700, 20609294, 100990000000]]
+World = pd.DataFrame(data, columns=['name', 'continent', 'area', 'population', 'gdp']).astype({'name':'object', 'continent':'object', 'area':'Int64', 'population':'Int64', 'gdp':'Int64'})
+print(World)
+big_countries=World[(World['area']>=3000000) | (World['population']>=25000000)]
+result=big_countries[['name','population','area']]
+print(result)
+
+
+
+
+
